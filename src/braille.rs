9108b38fa2d3602c1840e48b1d723fbdb06aae63
Patrick Burroughs (Celti) <celti@celti.name>
2016-05-06 23:37:35 -0700

    v0.9.11

--- src/braille.rs
+++ src/braille.rs
@@ -5,21 +5,31 @@
 // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
 // This file may not be copied, modified, or distributed except
 // according to those terms.
+
+use std::cmp::max;
 use super::graph::{Result, ParseGraphError};
 
 const EMPTY: u32 = 0x2800;
-const CHAR: [&'static [&'static [u32]]; 2] =
+const CHAR: [&'static [&'static [u32]]; 4] =
     [ &[ &[0x00, 0x40, 0x44, 0x46, 0x47],
-            &[0x00, 0x80, 0xA0, 0xB0, 0xB8] ],
-        &[ &[0x00, 0x01, 0x09],
-            &[0x00, 0x02, 0x12],
-            &[0x00, 0x04, 0x24],
-            &[0x00, 0x40, 0xC0] ] ];
+         &[0x00, 0x80, 0xA0, 0xB0, 0xB8],],
+      &[ &[0x00, 0x01, 0x09],
+         &[0x00, 0x02, 0x12],
+         &[0x00, 0x04, 0x24],
+         &[0x00, 0x40, 0xC0],],
+      &[ &[0x00, 0x01, 0x03, 0x07, 0x47], 
+         &[0x00, 0x08, 0x18, 0x38, 0xB8],],
+      &[ &[0x00, 0x08, 0x09],
+         &[0x00, 0x10, 0x12],
+         &[0x00, 0x20, 0x24],
+         &[0x00, 0x80, 0xC0],],
+    ];
 
-pub fn horizontal_graph(input: Vec<usize>) -> Result<Vec<Vec<u32>>> {
+pub fn horizontal_graph(reverse: bool, input: Vec<usize>) -> Result<Vec<Vec<u32>>> {
     let blocks     = input.len() / 2 + if input.len() % 2 > 0 { 1 } else { 0 };
-    let lines      = (try!(input.iter().max().ok_or(ParseGraphError)) + 3) / 4;
+    let lines      = max((try!(input.iter().max().ok_or(ParseGraphError)) + 3) / 4, 1);
     let mut output = vec![vec![EMPTY; blocks]; lines];
+    let char       = if reverse { 2 } else { 0 };
 
     for (block,chunk) in input.chunks(2).collect::<Vec<&[usize]>>().iter().enumerate() {
         for (index,value) in chunk.iter().enumerate() {
@@ -27,20 +37,23 @@ pub fn horizontal_graph(input: Vec<usize>) -> Result<Vec<Vec<u32>>> {
                 let partial = value.saturating_sub(line * 4);
                 let insert = if partial > 4 { 4 } else { partial };
 
-                output[line][block] += CHAR[0][index][insert]
+                output[line][block] += CHAR[char][index][insert]
             }
         }
     };
 
-    output.reverse();
+    if !reverse {
+        output.reverse()
+    };
 
     Ok(output)
 }
 
-pub fn vertical_graph(input: Vec<usize>) -> Result<Vec<Vec<u32>>> {
-    let blocks     = (try!(input.iter().max().ok_or(ParseGraphError)) + 1) / 2;
+pub fn vertical_graph(reverse: bool, input: Vec<usize>) -> Result<Vec<Vec<u32>>> {
+    let blocks     = max((try!(input.iter().max().ok_or(ParseGraphError)) + 1) / 2, 1);
     let lines      = input.len() / 4 + if input.len() % 4 > 0 { 1 } else { 0 };
     let mut output = vec![vec![EMPTY; blocks]; lines];
+    let char       = if reverse { 3 } else { 1 };
 
     for (line, chunk) in input.chunks(4).collect::<Vec<&[usize]>>().iter().enumerate() {
         for (index,value) in chunk.iter().enumerate() {
@@ -48,8 +61,14 @@ pub fn vertical_graph(input: Vec<usize>) -> Result<Vec<Vec<u32>>> {
                 let partial = value.saturating_sub(block * 2);
                 let insert = if partial > 2 { 2 } else { partial };
 
-                output[line][block] += CHAR[1][index][insert]
+                output[line][block] += CHAR[char][index][insert]
             }
+        }
+    };
+
+    if reverse {
+        for line in 0..lines {
+            output[line].reverse()
         }
     };
 
---
+++ tests/braille_graph.rs
@@ -0,0 +1,78 @@
+// Copyright (c) 2016 Patrick Burroughs <celti@celti.name>.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0>, or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
+// This file may not be copied, modified, or distributed except
+// according to those terms.
+
+extern crate unicode_graph;
+use unicode_graph::braille::{vertical_graph, horizontal_graph};
+
+#[test]
+fn vertical_graph_full_eight_by_eight() {
+    let input:  Vec<usize>    = vec![8; 8];
+    let expect: Vec<Vec<u32>> = vec![vec![0x28FF; 4]; 2];
+    assert!( vertical_graph(false, input.clone()).unwrap().eq(&expect) );
+    assert!( vertical_graph(true,  input.clone()).unwrap().eq(&expect) );
+}
+
+#[test]
+fn vertical_graph_empty_eight_by_eight() {
+    let input:  Vec<usize>    = vec![0; 8];
+    let expect: Vec<Vec<u32>> = vec![vec![0x2800]; 2];
+    assert!( vertical_graph(false, input.clone()).unwrap().eq(&expect) );
+    assert!( vertical_graph(true,  input.clone()).unwrap().eq(&expect) );
+}
+
+#[test]
+fn vertical_graph_incrementing_one_to_eight() {
+    let input:    Vec<usize>    = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    let expect:   Vec<Vec<u32>> = vec![vec![0x28F7, 0x28C4, 0x2800, 0x2800], vec![0x28FF, 0x28FF, 0x28F7, 0x28C4]];
+    let expect_r: Vec<Vec<u32>> = vec![vec![0x2800, 0x2800, 0x28E0, 0x28FE], vec![0x28E0, 0x28FE, 0x28FF, 0x28FF]];
+    assert!( vertical_graph(false, input.clone()).unwrap().eq(&expect)   );
+    assert!( vertical_graph(true,  input.clone()).unwrap().eq(&expect_r) );
+}
+
+#[test]
+fn vertical_graph_decrementing_eight_to_one() {
+    let input:    Vec<usize>    = vec![8, 7, 6, 5, 4, 3, 2, 1];
+    let expect:   Vec<Vec<u32>> = vec![vec![0x28FF, 0x28FF, 0x287F, 0x280B], vec![0x287F, 0x280B, 0x2800, 0x2800]];
+    let expect_r: Vec<Vec<u32>> = vec![vec![0x2819, 0x28BF, 0x28FF, 0x28FF], vec![0x2800, 0x2800, 0x2819, 0x28BF]];
+    assert!( vertical_graph(false, input.clone()).unwrap().eq(&expect)   );
+    assert!( vertical_graph(true,  input.clone()).unwrap().eq(&expect_r) );
+}
+
+#[test]
+fn horizontal_graph_full_eight_by_eight() {
+    let input:  Vec<usize>    = vec![8; 8];
+    let expect: Vec<Vec<u32>> = vec![vec![0x28FF; 4]; 2];
+    assert!( horizontal_graph(false, input.clone()).unwrap().eq(&expect) );
+    assert!( horizontal_graph(true,  input.clone()).unwrap().eq(&expect) );
+}
+
+#[test]
+fn horizontal_graph_empty_eight_by_eight() {
+    let input:  Vec<usize>    = vec![0; 8];
+    let expect: Vec<Vec<u32>> = vec![vec![0x2800; 4]; 1];
+    assert!( horizontal_graph(false, input.clone()).unwrap().eq(&expect) );
+    assert!( horizontal_graph(true,  input.clone()).unwrap().eq(&expect) );
+}
+
+#[test]
+fn horizontal_graph_incrementing_one_to_eight() {
+    let input:    Vec<usize>    = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    let expect:   Vec<Vec<u32>> = vec![vec![0x2800, 0x2800, 0x28E0, 0x28FE], vec![0x28E0, 0x28FE, 0x28FF, 0x28FF]];
+    let expect_r: Vec<Vec<u32>> = vec![vec![0x2819, 0x28BF, 0x28FF, 0x28FF], vec![0x2800, 0x2800, 0x2819, 0x28BF]];
+    assert!( horizontal_graph(false, input.clone()).unwrap().eq(&expect)   );
+    assert!( horizontal_graph(true,  input.clone()).unwrap().eq(&expect_r) );
+}
+
+#[test]
+fn horizontal_graph_decrementing_eight_to_one() {
+    let input:    Vec<usize>    = vec![8, 7, 6, 5, 4, 3, 2, 1];
+    let expect:   Vec<Vec<u32>> = vec![vec![0x28F7, 0x28C4, 0x2800, 0x2800], vec![0x28FF, 0x28FF, 0x28F7, 0x28C4]];
+    let expect_r: Vec<Vec<u32>> = vec![vec![0x28FF, 0x28FF, 0x287F, 0x280B], vec![0x287F, 0x280B, 0x2800, 0x2800]];
+    assert!( horizontal_graph(false, input.clone()).unwrap().eq(&expect)   );
+    assert!( horizontal_graph(true,  input.clone()).unwrap().eq(&expect_r) );
+}
